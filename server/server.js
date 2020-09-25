a67f0dbd88ce12bd39043ba86f8f30a29bf1f10d
juan <jcfuentest@gmail.com>
2020-09-25 15:52:09 -0300

    primer commit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+require('./config/config')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+
+    let body = req.body;
+    res.json({
+        body
+    })
+})
+app.get('/usuario', function(req, res) {
+    res.json('get app usuario')
+})
+
+app.post('/usuario', function(req, res) {
+    let body = req.body
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            message: 'el nombre debe estar si o si'
+        })
+
+    } else {
+        res.json({
+            body
+        })
+    }
+
+
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log('escuchando al puerto 3000')
+})
